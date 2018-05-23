b5c4c7082bab331f43d2ee23b21c5cf5df2593dc
chinhqn <chinhsoftoneqn@gmail.com>
2018-05-23 17:28:48 +0700

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+
+
+module.exports = {
+    entry : {
+        bundle : './src/index.js',
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module : {
+        rules : [
+            {
+                use: 'babel-loader',
+                test: /\.js$/,
+                exclude: '/node_modules/'
+            },
+            {
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+                test: /\.css$/
+            },
+            {
+                loader: 'file-loader',
+                test: /\.(png|jpg|gif)$/
+            }
+        ]
+    }
+}
